9d7dc76bf52391cffcaabc36fe9be6e3c7d71662
MatiasC5 <cardosomatias1994@gmail.com>
2024-05-01 18:50:36 -0300

    Proyeto practica frontend. Newsletter signup with success message.

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import { SignUp } from "./components/SignUp/SignUp.tsx";
+
+export const App = () => {
+  return <SignUp />;
+};
---
+++ src/components/SignUp/SignUp.tsx
@@ -0,0 +1,43 @@
+import SigUpIllustration from "../../assets/images/illustration-sign-up-desktop.svg";
+import IconList from "../../assets/images/icon-list.svg";
+import "./SignUp.css";
+
+export const SignUp: React.FC = () => {
+  return (
+    <div className="signup">
+      <section className="signup-info">
+        <h1>Stay updated!</h1>
+        <p>Join 60,000+ product managers receiving monthly updates on:</p>
+        <ul>
+          <li>
+            {" "}
+            <img src={IconList} alt="list-icon" /> Product discovery and
+            building what matters
+          </li>
+          <li>
+            <img src={IconList} alt="list-icon" /> Measuring to ensure updates
+            are a success
+          </li>
+          <li>
+            <img src={IconList} alt="list-icon" />
+            And much more!
+          </li>
+        </ul>
+
+        <form>
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            placeholder="email@company.com"
+            id="email"
+            autoComplete="off"
+          />
+          <button type="submit"> Subscribe to monthly newsletter</button>
+        </form>
+      </section>
+      <section className="sign-up-illustration">
+        <img src={SigUpIllustration} alt="sign-up-illustration" />
+      </section>
+    </div>
+  );
+};
